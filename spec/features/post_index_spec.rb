c761c0f6ac75ac04d518ef6903e1760bdbb0886f
Optimus <github email address>
2018-03-10 01:59:09 -0800

    added restrictions for editing and deleting a post at the UI level

--- spec/features/post_index_spec.rb
+++ spec/features/post_index_spec.rb
@@ -3,9 +3,9 @@ require 'rails_helper'
 RSpec.feature "Index of Blog Posts" do
 
   before do
-    user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password")
-    @post1 = Post.create!(title: "Game of Thrones is the best show ever!", body: "I can go on and on about this show", user: user)
-    @post2 = Post.create!(title: "Can't stop coding", body: "I love creating projects with the things I've learned about coding!", user: user)
+    @user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password")
+    @post1 = Post.create!(title: "Game of Thrones is the best show ever!", body: "I can go on and on about this show", user: @user)
+    @post2 = Post.create!(title: "Can't stop coding", body: "I love creating projects with the things I've learned about coding!", user: @user)
   end
 
   scenario "An index of blog posts is shown" do
@@ -16,6 +16,19 @@ RSpec.feature "Index of Blog Posts" do
     expect(page).to have_link(@post1.title)
     expect(page).to have_link(@post2.title)
 
+  end
+
+  scenario "New post button shows up if user is logged in" do
+    login_as(@user)
+    visit root_path
+
+    expect(page).to have_link("New Post")
+  end
+
+  scenario "New post button doesn't show if user not signed in" do
+    visit root_path
+
+    expect(page).not_to have_link("New Post")
   end
 
   scenario "No blog posts for index" do
--- spec/features/post_show_spec.rb
+++ spec/features/post_show_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 
 RSpec.feature "Show a Post on its own page" do
   before do
-    user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password")
-    login_as(user)
-    @post = Post.create(title: "This is a show page post title", body: "This is the post body for our show page test", user: user)
+    @user = User.create!(email: "test@test.com", password: "password", password_confirmation: "password")
+    @user2 = User.create!(email: "test2@test.com", password: "password", password_confirmation: "password")
+    @post = Post.create(title: "This is a show page post title", body: "This is the post body for our show page test", user: @user)
   end
 
   scenario "A user can view a Post's show page" do
@@ -14,5 +14,31 @@ RSpec.feature "Show a Post on its own page" do
     expect(page).to have_content(@post.title)
     expect(page).to have_content(@post.body)
     expect(current_path).to eq(post_path(@post))
+  end
+
+  scenario "A user can't see the edit or delete button unless signed in" do
+    visit root_path
+    click_link @post.title
+
+    expect(page).not_to have_link("Edit Post")
+    expect(page).not_to have_link("Delete Post")
+  end
+
+  scenario "A user can only see edit and delete post buttons if they created the post" do
+    login_as(@user2)
+    visit root_path
+    click_link @post.title
+
+    expect(page).not_to have_link("Edit Post")
+    expect(page).not_to have_link("Delete Post")
+  end
+
+  scenario "An owner can see the edit and delete buttons on show page" do
+    login_as(@user)
+    visit root_path
+    click_link @post.title
+
+    expect(page).to have_link("Edit Post")
+    expect(page).to have_link("Delete Post")
   end
 end
